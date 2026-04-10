c4390e172801822a966560991702e77a2c502fc5
HopewellITdept <ITdept@hopewellva.gov>
2026-04-10 08:32:51 -0400

    Update link in EventCalendarEnhanced component

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -207,7 +207,7 @@ const EventCalendarEnhanced = () => {
                 </td> 
  <td style={{ width: '100px', border: 'none', padding: '5px' }}>   
 <a
-                  href="https://itdept9800013c97-dkqyi.wordpress.com/about/" 
+                  href="https://itdept9800013c97-dkqyi.wordpress.com/" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="block max-w-xs"
