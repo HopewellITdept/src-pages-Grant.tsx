44337ca9ac56a90ebca38182e3ee184fdf89b553
HopewellITdept <ITdept@hopewellva.gov>
2026-05-04 12:47:49 -0400

    Adjust table cell width in EventCalendarEnhanced

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -221,7 +221,7 @@ const EventCalendarEnhanced = () => {
                   /><figcaption><strong>General Ulysses S. Grant at City Point</strong></figcaption></figure>
                 </a>
 </td> 
-<td style={{ width: '30px', border: 'none', padding: '2px' }}>        
+<td style={{ width: '10px', border: 'none', padding: '2px' }}>        
                 <a
                   href="https://itdept9800013c97-dkqyi.wordpress.com/historic-hopewell-foundation-a-celebration-of-americas-250/" 
                   target="_blank" 
