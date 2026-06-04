d29e05c77feffdfb8bd4d8d30757b32470305dd2
HopewellITdept <ITdept@hopewellva.gov>
2026-06-04 19:49:39 -0400

    Refactor event filtering and clean up code

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -11,18 +11,14 @@ const EventCalendarEnhanced = () => {
   // Empty events array - all events to be announced
   const events: any[] = []; 
 
-const eventTypes = [
+  const eventTypes = [
     { value: 'all', label: 'All Events' },
     { value: 'family', label: 'Family' },
     { value: 'history', label: 'History' },
     { value: 'community', label: 'Community' },
     { value: 'music', label: 'Music' }
   ];
-  
-  const filteredEvents = selectedFilter === 'all' 
-    ? events 
-    
-    : events.filter(event => event.type === selectedFilter);
+
   const filteredEvents = selectedFilter === 'all' 
     ? events 
     : events.filter(event => event.type === selectedFilter);
@@ -114,9 +110,6 @@ const eventTypes = [
               Month View
             </motion.button>
           </div>
-
-          
-          {/* View Toggle */}
 
 
           {/* Filter */}
