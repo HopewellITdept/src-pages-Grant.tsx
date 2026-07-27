ee8877938bc5ecc3afd14e03f3d8ad63434c3419
HopewellITdept <ITdept@hopewellva.gov>
2026-07-27 10:23:51 -0400

    Update image source for Weston Manor

--- src/components/EventCalendarEnhanced.tsx
+++ src/components/EventCalendarEnhanced.tsx
@@ -276,7 +276,7 @@ const EventCalendarEnhanced = () => {
                   className="block max-w-xs"
                 >
                   <figure><img
-                    src="/lovable-uploads/Weston%20Manor%20-2.jpg"
+                    src="/lovable-uploads/Weston%20Manor%202.png"
                     alt="Photo of a Weston Manor"
                     className="w-full rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
                     title="Weston Manor"
